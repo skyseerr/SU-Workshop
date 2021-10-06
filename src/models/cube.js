8934557c05265ddcfa11ac5a416568195a6627eb
skyseerr <mr.ivanasenov@gmail.com>
2021-10-06 15:46:58 +0300

    Moved static DB in class

--- src/models/cube.js
+++ src/models/cube.js
@@ -1,9 +1,27 @@
 class Cube {
-    constructor(name, description, imageUrl, difficulty){
+
+    static cubes = [
+        {
+            name: 'Mirror Cube',
+            description: 'Some description',
+            imageUrl: 'https://m.media-amazon.com/images/I/61xKF56d15L._SL1000_.jpg',
+            difficulty: '5'
+        },
+    ];
+
+    constructor(name, description, imageUrl, difficulty) {
         this.name = name;
         this.description = description;
         this.imageUrl = imageUrl;
         this.difficulty = difficulty;
+    }
+
+    static getAll() { 
+        return Cube.cubes.slice();
+    }
+
+    static add(cube) {
+        Cube.cubes.push(cube);
     }
 }
 
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,29 +1,18 @@
 const Cube = require('../models/cube');
 
-const cubeDb = [
-    {
-        name: 'Mirror Cube',
-        description: 'Some description',
-        imageUrl: 'https://m.media-amazon.com/images/I/61xKF56d15L._SL1000_.jpg',
-        difficulty: '5'
-    }
-];
-
-const getAll = () => cubeDb.slice();
+const getAll = () => Cube.getAll();
 
 const create = (name, description, imageUrl, difficulty) => {
 
     let cube = new Cube(name, description, imageUrl, difficulty);
 
-    cubeDb.push(cube);
-
+    Cube.add(cube);
 };
 
 const cubeServise = {
     create,
     getAll,
 };
-
 
 module.exports = cubeServise;
 
