02a77022e79f8791793929d4012efc3047415469
skyseerr <mr.ivanasenov@gmail.com>
2021-10-06 11:45:57 +0300

    add controllers

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const path = require('path');
-
-// const initHandlebars = require('./config/handlebars');
+const routes = require('./routes');
 
 const app = express();
 
 require('./config/handlebars')(app);
 
 app.use(express.static(path.resolve(__dirname, './public')));
+app.use(routes);
 
-// initHandlebars(app); or require above
 
-app.all('/', (req, res) => {
-    res.render('index');
-});
 
 app.listen(5000, console.log.bind(console, 'Application is running on http://localhost:5000'));
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const homeController = require('./controllers/homeController');
+const cubeController = require('./controllers/cubeController');
+
+const router = express.Router();
+
+router.use(homeController);
+router.use('/cube', cubeController);
+
+module.exports = router;
