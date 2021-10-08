9a4c5c2ffa02c00d3ca8700f0cfa8e58d66f6a55
skyseerr <mr.ivanasenov@gmail.com>
2021-10-08 12:39:49 +0300

    Add about page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -11,6 +11,10 @@ const home = (req, res) => {
     res.render('index', { cubes });
 };
 
-router.get('/', home);
+const about = (req, res) =>{
+    res.render('about');
+};
 
+router.get('/', home);
+router.get('/about', about);
 module.exports = router;
