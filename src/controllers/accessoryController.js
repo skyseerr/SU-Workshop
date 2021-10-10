fc28c93ebb7458bc4ed8765ac793cc9d8b1cc19c
skyseerr <mr.ivanasenov@gmail.com>
2021-10-10 12:53:11 +0300

    Add link for create accessory page in nav

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
 
 router.get('/create', (req, res) => {
-
     res.render('accessory/create');
-
 });
+
+router.post('/create', (req, res) => {
+    let accessory = req.body;
+    console.log(accessory);
+    res.redirect('/');
+});
+
 
 module.exports = router;
